f852b59f6b57c9e408a10e0d36a1d4187c85bf52
Eduardo Almeida <oeduardoal@gmail.com>
2021-10-21 18:55:09 -0300

    feat: 🎸 add page

--- src/account.tsx
+++ src/account.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container } from "@grupoboticario/flora-react-container";
+import { Heading } from "@grupoboticario/flora-react-heading";
+import { useLocation } from "react-router-dom";
 
-const Account: React.FC = ({ name = "Not logged" }) => {
-  return <>{name ? `Welcome ${name}` : ""}</>;
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const Account = () => {
+  const query = useQuery();
+  const name = query.get("name");
+
+  return (
+    <Container size="large" css={{ paddingTop: "$8" }}>
+      <Heading level={5} css={{ fontWeight: "$strong" }}>
+        Olá {name}
+      </Heading>
+    </Container>
+  );
 };
 
 export default Account;
